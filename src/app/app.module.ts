3df7fd4472bd06937716065440cd411cd742b945
alancrps <alanfcorpus@gmail.com>
2023-02-27 22:15:03 -0300

    Terminada estructura de la página
    Terminada la parte del front end en su mayoría.
    -Se agregaron los componentes, imágenes y estilos necesarios para el proyecto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderNavComponent } from './components/header-nav/header-nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationAndExperienceComponent } from './components/education-and-experience/education-and-experience.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderNavComponent,
+    BannerComponent,
+    AboutmeComponent,
+    SkillsComponent,
+    EducationAndExperienceComponent,
+    ProyectsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/education-and-experience/education-and-experience.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-education-and-experience',
+  templateUrl: './education-and-experience.component.html',
+  styleUrls: ['./education-and-experience.component.css']
+})
+export class EducationAndExperienceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
